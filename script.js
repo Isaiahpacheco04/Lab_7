9fc371455c3d0928907e78aa1fa0d4980abb20b2
Isaiahpacheco04 <156877323+Isaiahpacheco04@users.noreply.github.com>
2025-11-12 23:45:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-function init(){
-//add your javascrip between these two lines of code
- 
-    // get elements
-    const button = document.getElementById("entrybutton");
-    const input = document.getElementById("entryinput");
-    const output = document.getElementById("textoutput");
+function init() {
+    const input = document.getElementById('entryinput');
+    const button = document.getElementById('entrybutton');
+    const output = document.getElementById('textoutput');
 
-    // add click event to the Alert Me button
-    button.addEventListener("click", function () {
-
-        const text = input.value;   // whatever user typed
-
-        // 1. show alert with your name + colon + text
+    button.addEventListener('click', function () {
+        let text = input.value;
         alert("Isaiah Pacheco: " + text);
-
-        // 2. update the h2 text to JUST the input text
         output.textContent = text;
-
     });
-
 }
 
 window.addEventListener('load', init);
-
